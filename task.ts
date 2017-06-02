465cb62db589254b7b2621ee0edd0e546c2fa7e9
Noman Ahmed <nomanrajput784@gmail.com>
2017-06-02 23:10:11 +0500

    firstcommit

---
+++ task.ts
@@ -0,0 +1,33 @@
+/* let test: string ='Hello';
+// let test: number = 5;
+// let test: boolean = true;  
+// let test: any={}; (any of the type is allowed to use)
+//let test: string | number = 'asdas';
+//let testArr : string[] = ['asda'];
+
+
+
+//console.log(`${test}world`);
+console.log(typeof test);
+
+function add(numOne, numTwo) void {
+return numOne + numOne
+}
+console.log(add(5,5));
+
+*/
+/*
+
+var str1 = new String( "This is string one" );  
+var index = str1.indexOf( "string" ); 
+console.log("indexOf found String :" + index );
+
+
+var index = str1.indexOf( "one" ); 
+console.log("indexOf found String :" + index ); */
+
+//let announcement: string ='My name is Umair';
+
+//console.log(announcement);
+
+
